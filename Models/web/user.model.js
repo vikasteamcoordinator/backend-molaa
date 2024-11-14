29c97a5f9a2744e983ef22d8f37842f2f38ee6a3
Vikash Tiwari <teamcoordinator@earninghandle.com>
2024-11-14 16:54:45 +0530

    required

--- Models/web/user.model.js
+++ Models/web/user.model.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true},
   password: { type: String, required: true },
   photo: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
